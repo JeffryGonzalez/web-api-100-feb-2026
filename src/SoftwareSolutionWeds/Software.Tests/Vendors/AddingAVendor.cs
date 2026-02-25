9b849e27cc8b7b5fedbd0d979ae3e8af2837fd6a
Jeffry Gonzalez <jeff@hypertheory>
2026-02-25 09:23:13 -0500

    prep

--- src/SoftwareSolutionWeds/Software.Tests/Vendors/AddingAVendor.cs
+++ src/SoftwareSolutionWeds/Software.Tests/Vendors/AddingAVendor.cs
@@ -1,37 +1,37 @@
-﻿using Alba;
-using Marten;
-using Microsoft.Extensions.DependencyInjection;
-using Software.Api.Vendors;
-using Software.Tests.Fixtures;
+﻿// using Alba;
+// using Marten;
+// using Microsoft.Extensions.DependencyInjection;
+// using Software.Api.Vendors;
+// using Software.Tests.Fixtures;
 
 
-namespace Software.Tests.Vendors;
+// namespace Software.Tests.Vendors;
 
-[Collection("SoftwareSystemTestCollection")]
-public class AddingAVendor(SoftwareSystemTestFixture fixture) : IClassFixture<SoftwareSystemTestFixture>
-{
+// [Collection("SoftwareSystemTestCollection")]
+// public class AddingAVendor(SoftwareSystemTestFixture fixture) : IClassFixture<SoftwareSystemTestFixture>
+// {
 
-    [Fact]
-    public async Task CanAddVendor()
-    {
-        var vendorToPost = new CreateVendorRequestModel { Name = "Test Vendor", Url = "http://testvendor.com", PointOfContact = new VendorPointOfContactModel { Name = "John Doe", Email = "joe@aol.com", Phone="867-5309" } };
-        var response = await fixture.Host.Scenario(api =>
-        {
-            api.Post.Json(vendorToPost).ToUrl("/vendors");
-            api.StatusCodeShouldBeOk();
-        });
-
-
-        // We could check the database
-        using var scope = fixture.Host.Services.CreateScope();
-        var session = scope.ServiceProvider.GetRequiredService<IDocumentSession>();
+//     [Fact]
+//     public async Task CanAddVendor()
+//     {
+//         var vendorToPost = new CreateVendorRequestModel { Name = "Test Vendor", Url = "http://testvendor.com", PointOfContact = new VendorPointOfContactModel { Name = "John Doe", Email = "joe@aol.com", Phone="867-5309" } };
+//         var response = await fixture.Host.Scenario(api =>
+//         {
+//             api.Post.Json(vendorToPost).ToUrl("/vendors");
+//             api.StatusCodeShouldBeOk();
+//         });
 
 
-        var vendor = await session.Query<VendorEntity>().FirstOrDefaultAsync(v => v.Name == vendorToPost.Name, TestContext.Current.CancellationToken);
+//         // We could check the database
+//         using var scope = fixture.Host.Services.CreateScope();
+//         var session = scope.ServiceProvider.GetRequiredService<IDocumentSession>();
 
-        // Why would we do this? What would we look for?
-        // Another way?
 
-        Assert.NotNull(vendor);
-    }
-}
+//         var vendor = await session.Query<VendorEntity>().FirstOrDefaultAsync(v => v.Name == vendorToPost.Name, TestContext.Current.CancellationToken);
+
+//         // Why would we do this? What would we look for?
+//         // Another way?
+
+//         Assert.NotNull(vendor);
+//     }
+// }
